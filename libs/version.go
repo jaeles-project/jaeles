fd1877465f810d2d813fdc630312c8951eff2d99
j3ssie <jeromej3m@gmail.com>
2020-04-05 21:09:06 +0700

    v0.7 release

--- libs/version.go
+++ libs/version.go
@@ -2,7 +2,7 @@ package libs
 
 const (
 	// VERSION current Jaeles version
-	VERSION = "beta v0.6.3"
+	VERSION = "beta v0.7"
 	// AUTHOR author of this
 	AUTHOR = "@j3ssiejjj"
 	// SIGNREPO default repo to get signature

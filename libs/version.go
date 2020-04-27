e52ef7be20942a72c340463d8841ebbf2a2a78ea
j3ssie <jeromej3m@gmail.com>
2020-04-27 09:53:48 +0700

    v0.8 Release

--- libs/version.go
+++ libs/version.go
@@ -2,7 +2,7 @@ package libs
 
 const (
 	// VERSION current Jaeles version
-	VERSION = "beta v0.7.6"
+	VERSION = "beta v0.8"
 	// AUTHOR author of this
 	AUTHOR = "@j3ssiejjj"
 	// SIGNREPO default repo to get signature
